e8ed479619fd01c350c5939158ae5735771ec9fa
AlbGouveia <joao.gouveia@protonmail.com>
2021-05-25 05:23:10 -0300

    código principal

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+from Token import TokenDiscord
+import os
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='.')
+
+
+@client.command()
+@commands.is_owner()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send(f'Extensão {extension} carregada.')
+
+
+@client.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'Extensão {extension} descarregada.')
+
+
+@client.command()
+@commands.is_owner()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send(f'Extensão {extension} recarregada.')
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run(TokenDiscord.uploadToken()['token'])
